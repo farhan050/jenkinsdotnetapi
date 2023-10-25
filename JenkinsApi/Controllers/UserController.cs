c5096c46a207f0fb5b731a4051dfed4837cb08cf
Farhan050 <farhan.irfan@braincrop.net>
2023-10-25 12:10:32 +0500

    feat: user controller add

---
+++ JenkinsApi/Controllers/UserController.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace JenkinsApi.Controllers
+{
+    [Route("api/[controller]/[action]")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+        [HttpPost]
+        public IActionResult CreateUser([FromBody] string Username)
+        {
+            return Ok(Username);
+        }
+        [HttpGet]
+        public IActionResult GetUser()
+        {
+            return Ok("Farhan");
+        }
+        [HttpGet]
+        public IActionResult GetUserbyid([FromQuery] int id)
+        {
+            return Ok("Farhan");
+        }
+        [HttpPut]
+        public IActionResult UpdateUserbyid([FromQuery] int id)
+        {
+            return Ok("Hello");
+        }
+        [HttpDelete]
+        public IActionResult deleteUserbyid([FromQuery] int id)
+        {
+            return Ok("Hello");
+        }
+
+
+    }
+}
